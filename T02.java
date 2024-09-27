1c3ac36bfa958e39d8dcd4dd5a48d5d37af4a6de
Choqy Pananda Sirait <krisky325@gmail.com>
2024-09-27 10:51:59 +0700

    T02-Pair 11
    Kami telah menyelesaikan tugas praktikum

--- T02.java
+++ T02.java
@@ -1,1 +1,44 @@
-// NIM - Your Name
+// 12S24006 - Margareth Bungaran Sitompul   
+// 12S24012 - Choqy Pananda Sirait
+import java.util.*;
+import java.lang.Math;
+
+public class JavaApplication {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN, judul, penerbit, triple, ket, ket1, ket2;
+        int tahun, stok;
+        double hargaP, minimumM, rating;
+
+        iSBN = input.nextLine();
+        judul = input.nextLine();
+        penerbit = input.nextLine();
+        tahun = Integer.parseInt(input.nextLine());
+        triple = input.nextLine();
+        ket = input.nextLine();
+        hargaP = Double.parseDouble(input.nextLine());
+        minimumM = Double.parseDouble(input.nextLine());
+        rating = Double.parseDouble(input.nextLine());
+        if (rating >= 4.7 || rating <= 5.0) {
+            ket1 = "Best pick";
+        } else {
+            if (rating >= 4.5 || rating < 4.7) {
+                ket1 = "Must read";
+            } else {
+                if (rating >= 4.0 || rating < 4.5) {
+                    ket1 = "Recomended";
+                } else {
+                    if (rating >= 3.0 || rating < 4.0) {
+                        ket1 = "Average";
+                    } else {
+                        if (rating < 3.0 || rating >= 0.0) {
+                            ket1 = "low";
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(iSBN + "|" + judul + "|" + penerbit + "|" + tahun + "|" + triple + "|" + ket + "|" + hargaP + "|" + minimumM + "|" + rating + "|" + ket1);
+    }
+}
